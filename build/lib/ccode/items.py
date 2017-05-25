d586176c467e47b7e18dd74d514821ccd57d30ae
shashank <shashank.sharma98@gmail.com>
2017-05-25 15:24:59 +0530

    Developed Scrapper - Testing phase

--- build/lib/ccode/items.py
+++ build/lib/ccode/items.py
@@ -10,11 +10,5 @@ import scrapy
 
 class CcodeItem(scrapy.Item):
     # define the fields for your item here like:
-    name = scrapy.Field()
-    code = scrapy.Field()
-    successful_submission = scrapy.Field()
-    accuracy = scrapy.Field()
-    answer = scrapy.Field()
-
-class Answer(scrapy.Item):
-	pass
+    # name = scrapy.Field()
+    pass
--- ccode/spiders/slink.py
+++ ccode/spiders/slink.py
@@ -1,86 +1,91 @@
+'''
+Codechef spider
+
+Scrapy spider for ScrapingHub project.
+Enter start_urls with the given url and rest it will do it for you.
+
+1. Grab all the name, coden submission and accuracy.
+2. Join URL and code then check the solution of each problem
+3. Solutions are divided into 4 parts with max 50 answers each.
+  - C
+  - C++
+  - Java
+  - Python
+4. After getting answer it will append to the list and yield it.
+
+'''
+
 import scrapy
 
 class LoginSpider(scrapy.Spider):
 
     def __init__(self):
+
+        # To first append all the given details and use it later
+        # We could have passed the argument through callback
         self.name = []
         self.code = []
         self.successful_submission = []
         self.accuracy = []
+        #self.lang = {'114': ' GO', '20': ' D'}
+        self.lang = {'11': ' C', '44': ' C++14', '10': ' JAVA', '4': ' PYTH'}
         self.answer = []
         self.count = -1
+        self.count1 = 0
+        self.ll = ''
+        self.kk = ''
 
     name = 'codespider'
+
+    # URL to start scrapping
     start_urls = ['https://www.codechef.com/problems/school']
 
     def getAnswer(self, response):
-        answer = []
+        count1 = 0
+        data = response.meta['data']
         a = response.css('tbody')
-        for i in a.css('tr'):
-            answer.append(i.css('td::text').extract()[0])
-        self.count += 1
-        return {
-            'name': self.name[self.count],
-            'code': self.code[self.count],
-            'successful_submission': self.successful_submission[self.count],
-            'accuracy': self.accuracy[self.count],
-            'answer': answer,
-        }
-        '''
-        self.result = []
-        a = response.css('tbody')
-        for i in a.css('tr'):
-            self.result.append(i.css('td::text').extract()[0])
-        yield self.result
-        '''
+        if self.lang[self.ll] in response.css('select.field-style1 option::text').extract():
+            for i in a.css('tr'):
+                try:
+                    data['answer'][self.ll].append(i.css('td::text').extract()[0])
+                except:
+                    print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=')
+                    print(i.css('td::text').extract())
+                    print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=')
 
+            if ('/sites/all/themes/abessive/images/page-next-active.gif' in response.text):
+                count1 += 1
+                ans = 'https://www.codechef.com' + response.css('a::attr(href)').re(r'/status/.*')[0]
+                print('-=-=-=-=')
+                print(ans)
+                yield scrapy.Request(url = ans, callback = self.getAnswer, meta = {'data': data})
+        yield data
 
     def parse(self, response):
-        ll = []
-        #response.css('div.problemname a::attr(href)').re(r'problems.*')
         for link in response.css('tr.problemrow'):
-            self.name.append(link.css('div.problemname b::text').extract()[0])
-            self.code.append(link.css('td a::text').extract()[2])
-            self.successful_submission.append(link.css('td div::text').extract()[2])
-            self.accuracy.append(link.css('td a::text').extract()[3])
+            data = {}
+            data['name'] = link.css('div.problemname b::text').extract()[0]
+            data['code'] = link.css('td a::text').extract()[2]
+            data['successfully_submission'] = link.css('td div::text').extract()[2]
+            data['accuracy'] = link.css('td a::text').extract()[3]
+            data['answer'] = {}
 
-            ans = 'https://www.codechef.com/status/' + link.css('td a::text').extract()[2]
-            yield scrapy.Request(url = ans, callback = self.getAnswer)
+            '''
+            Join URL with the given CODE
 
+            Parameters used in URL:
 
+            ?sort_by=Time&sorting_order=asc&language=4&status=15&Submit=GO
 
+            sort_by = Sort the given data
+            sorting_order = Ascending or Descending
+            language = [4: Python], [116: Python3.4], [44: C++14], [11: C], [10: Java]
+            status = [15: Accepted]
+            submit = GO (default)
 
-'''
-
-
-import scrapy
-
-class LoginSpider(scrapy.Spider):
-    result = []
-
-    name = 'codespider'
-    start_urls = ['https://www.codechef.com/problems/school']
-
-    def getAnswer(self, response):
-        item = response.meta['item']
-        return item
-
-    def parse(self, response):
-        #response.css('div.problemname a::attr(href)').re(r'problems.*')
-        for link in response.css('tr.problemrow'):
-            ans = 'https://www.codechef.com/status/' + link.css('td a::text').extract()[2]
-            #answer = getAnswer(ans, self.)
-            #answer = scrapy.Request(ans, callback=self.getAnswer, dont_filter=True)
-            answer = scrapy.Request(ans, callback=self.getAnswer, dont_filter=True)
-            print('\n\n\n')
-            print(answer)
-            yield {
-                'name': link.css('div.problemname b::text').extract()[0],
-                'code': link.css('td a::text').extract()[2],
-                'successful_submission': link.css('td div::text').extract()[2],
-                'accuracy': link.css('td a::text').extract()[3],
-                'answer': self.result,
-            }
-
-
-'''
+            '''
+            for i in self.lang.keys():
+                ans = 'https://www.codechef.com/status/' + data['code'] + '?sort_by=Time&sorting_order=asc&language='+str(i)+'&status=15&Submit=GO'
+                data['answer'][str(i)] = []
+                self.ll = str(i)
+                yield scrapy.Request(url = ans, callback = self.getAnswer, meta = {'data': data})
